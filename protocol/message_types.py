29cf844dd76967899b4f368fa11655c05e3bf3b2
Floormatt89 <86789069+FloorMatt89@users.noreply.github.com>
2026-03-12 02:44:45 -0400

    Add(Protocol Folder): Files from README created and message_type.py completed

---
+++ protocol/message_types.py
@@ -0,0 +1,11 @@
+from enum import IntEnum
+
+class MessageType(IntEnum):
+    CHOKE = 0
+    UNCHOKE = 1
+    INTERESTED = 2
+    NOT_INTERESTED = 3
+    HAVE = 4
+    BITFIELD = 5
+    REQUEST = 6
+    PIECE = 7
